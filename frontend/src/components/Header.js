1ebff284bf4cd87a9f33308e569d8bfdcff50702
karthick <karthias93@gmail.com>
2024-05-01 20:20:59 +0530

    removed bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,113 +1,111 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {LinkContainer} from "react-router-bootstrap";
-import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
-import {logout} from "../actions/userActions";
+import { logout } from "../actions/userActions";
 import "../screens/homescreen.css";
 import Avatar from "./Avatar";
-import Logo from './blackcatlogo2.jpg'
-
-
-
-// document.getElementById("shopping_cart_icon");
+import Logo from './blackcatlogo2.jpg';
+import { Menu, Layout } from 'antd';
 
 const Header = () => {
 	const dispatch = useDispatch();
+	const { Header } = Layout;
 
 	const userLogin = useSelector((state) => state.userLogin);
-	const {userInfo} = userLogin;
+	const { userInfo } = userLogin;
 
 	const logoutHandler = () => {
 		dispatch(logout());
 	};
 
 	return (
-		<header>
-      <Navbar  variant='dark' expand='lg' className='navbar1'>
-        <Container className='navbar_container'>
-           <LinkContainer to="/">
-				<Navbar.Brand className="jumboH2 text-info"><img className="logo1" src={Logo} alt="blackcatlogo"></img></Navbar.Brand>
-			</LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className="me-auto" id="first_nav">
-            <LinkContainer to="/">
-				<Navbar.Brand className="jumboH2 text-info ">Home</Navbar.Brand>
-			</LinkContainer>
-			<LinkContainer to="/story">
-				<Navbar.Brand className="jumboH2 text-info">My Story</Navbar.Brand>
-			</LinkContainer>
-			<LinkContainer to="/contact">
-				<Navbar.Brand className="jumboH2 text-info">Contact Me</Navbar.Brand>
-			</LinkContainer>
-                  <NavDropdown title="Products" className="jumboH2 text-info" id="navbardropdown1">
-                  <LinkContainer to='/Necklaces/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Necklaces</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/Bracelets/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Bracelets</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/Pendants/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Pendants</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/Statementpieces/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Statement Pieces</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/jewelry/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Jewelry Sets</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/Trees/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Trees</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/Miscellaneous/1'>
-                    <NavDropdown.Item className="jumboH5 text-info">Miscellaneous</NavDropdown.Item>
-                  </LinkContainer>
+		<Header>
+			<Navbar variant='dark' expand='lg' className='navbar1'>
+				<Container className='navbar_container'>
+					<NavLink to="/">
+						<Navbar.Brand className="jumboH2 text-info"><img className="logo1" src={Logo} alt="blackcatlogo"></img></Navbar.Brand>
+					</NavLink>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className="me-auto" id="first_nav">
+							<NavLink to="/">
+								<Navbar.Brand className="jumboH2 text-info ">Home</Navbar.Brand>
+							</NavLink>
+							<NavLink to="/story">
+								<Navbar.Brand className="jumboH2 text-info">My Story</Navbar.Brand>
+							</NavLink>
+							<NavLink to="/contact">
+								<Navbar.Brand className="jumboH2 text-info">Contact Me</Navbar.Brand>
+							</NavLink>
+							<NavDropdown title="Products" className="jumboH2 text-info" id="navbardropdown1">
+								<NavLink to='/Necklaces/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Necklaces</NavDropdown.Item>
+								</NavLink>
+								<NavLink to='/Bracelets/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Bracelets</NavDropdown.Item>
+								</NavLink>
+								<NavLink to='/Pendants/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Pendants</NavDropdown.Item>
+								</NavLink>
+								<NavLink to='/Statementpieces/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Statement Pieces</NavDropdown.Item>
+								</NavLink>
+								<NavLink to='/jewelry/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Jewelry Sets</NavDropdown.Item>
+								</NavLink>
+								<NavLink to='/Trees/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Trees</NavDropdown.Item>
+								</NavLink>
+								<NavLink to='/Miscellaneous/1'>
+									<NavDropdown.Item className="jumboH5 text-info">Miscellaneous</NavDropdown.Item>
+								</NavLink>
 
-                </NavDropdown>
-          </Nav>
-          <Nav className="ml-auto">
+							</NavDropdown>
+						</Nav>
+						<Nav className="ml-auto">
 							{userInfo ? (
 								<div className="d-lg-flex justify-content-start ms-lg-5">
 									<Avatar size="40px" url={userInfo.avatar} className="" />
 									<NavDropdown title={userInfo.name} id="username">
-										<LinkContainer to="/profile">
+										<NavLink to="/profile">
 											<NavDropdown.Item>Profile</NavDropdown.Item>
-										</LinkContainer>
+										</NavLink>
 										<NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
 									</NavDropdown>
 								</div>
 							) : (
-								<LinkContainer to="/login">
+								<NavLink to="/login">
 									<Nav.Link>
 										<i className="fas fa-user"></i> Sign In
 									</Nav.Link>
-								</LinkContainer>
+								</NavLink>
 							)}
 							{userInfo && userInfo.isAdmin && (
 								<NavDropdown title="Admin" id="adminmenu">
-									<LinkContainer to="/admin/userlist">
+									<NavLink to="/admin/userlist">
 										<NavDropdown.Item>Users</NavDropdown.Item>
-									</LinkContainer>
-									<LinkContainer to="/admin/productlist">
+									</NavLink>
+									<NavLink to="/admin/productlist">
 										<NavDropdown.Item>Products</NavDropdown.Item>
-									</LinkContainer>
-									<LinkContainer to="/admin/orderlist">
+									</NavLink>
+									<NavLink to="/admin/orderlist">
 										<NavDropdown.Item>Orders</NavDropdown.Item>
-									</LinkContainer>
+									</NavLink>
 								</NavDropdown>
-								
+
 							)}
-							<LinkContainer to="/cart">
+							<NavLink to="/cart">
 								<Nav.Link>
 									<i className="fas fa-shopping-cart text-info" id="shopping_cart_icon"></i>
 								</Nav.Link>
-							</LinkContainer>
+							</NavLink>
 						</Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-		</header>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</Header>
 	);
 };
 
--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,32 +1,3 @@
-// import React from 'react'
-// import { Pagination } from 'react-bootstrap'
-// import { LinkContainer } from 'react-router-bootstrap'
-
-// const Paginate = ({ pages, page, category, isAdmin = false, keyword = '' }) => {
-//   return (
-//     pages > 1 && (
-//       <Pagination>
-//         {[...Array(pages).keys()].map((x) => (
-//           <LinkContainer
-//             key={x + 1}
-//             to={
-//               !isAdmin
-//                 ? keyword
-//                   ? `/search/${keyword}/page/${x + 1}`
-//                   : category ? `/${category}/${x + 1}` : `/page/${x + 1}`
-//                 : `/admin/productlist/${x + 1}`
-//             }
-//           >
-//             <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
-//           </LinkContainer>
-//         ))}
-//       </Pagination>
-//     )
-//   )
-// }
-
-// export default Paginate
-
 import React from 'react';
 import { Link } from 'react-router-dom'; // Assuming you're using react-router-dom for routing
 
