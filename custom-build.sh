5074f1d1089a7f149b1b0a96f4c5d00cb065f7c6
karthick <karthias93@gmail.com>
2024-05-02 20:38:50 +0530

    build

--- custom-build.sh
+++ custom-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Run the heroku-postbuild script
-NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend
+echo "Installing dependencies for frontend..."
+NPM_CONFIG_PRODUCTION=false npm install --prefix frontend
 
+echo "Running build script for frontend..."
+npm run build --prefix frontend
 # Run the build command
 vercel-build
