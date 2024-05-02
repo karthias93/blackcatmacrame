016061a6c371c93d26af5ffb2e1aa13ab8ff5161
karthick <karthias93@gmail.com>
2024-05-02 20:42:24 +0530

    postbuild issue

--- custom-build.sh
+++ custom-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Installing dependencies for frontend..."
 NPM_CONFIG_PRODUCTION=false npm install --prefix frontend
 
-echo "Running build script for frontend..."
 npm run build --prefix frontend
+
 # Run the build command
 vercel-build
