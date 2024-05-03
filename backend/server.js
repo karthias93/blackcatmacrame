7999c0e072a51fe235ac936317a005f8c49aeddf
karthick <karthias93@gmail.com>
2024-05-03 18:36:25 +0530

    output dir

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
+  console.log(__dirname, 'dirname')
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
