ae924509d823be5b73ab1b7a0c1b64a182e3026d
karthick <karthias93@gmail.com>
2024-05-07 21:16:46 +0530

    port issur

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.SERVER_PORT || 5000
 
 app.listen(
   PORT,
